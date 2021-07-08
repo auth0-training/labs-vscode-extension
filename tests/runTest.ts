e0842dfd619820872f90070073d0e6c4f0b5facf
Bobby Johnson <bobby.johnson@gmail.com>
2021-07-08 13:57:44 -0700

    setting test runner to wait for extensions to load

--- tests/runTest.ts
+++ tests/runTest.ts
@@ -11,31 +11,55 @@ async function main() {
     // The folder containing the Extension Manifest package.json
     // Passed to `--extensionDevelopmentPath`
     const extensionDevelopmentPath = path.resolve(__dirname, '../../');
+    const extensionInstallPath = path.resolve(__dirname, '../extensions');
 
     // The path to test runner
     // Passed to --extensionTestsPath
-    const extensionTestsPath = path.resolve(__dirname, './suite/index');
-    const vscodeExecutablePath = await downloadAndUnzipVSCode('1.55.2');
+    const extensionTestsPath = path.resolve(__dirname, './suite/index.js');
+    const vscodeExecutablePath = await downloadAndUnzipVSCode('1.57.0');
     const cliPath =
       resolveCliPathFromVSCodeExecutablePath(vscodeExecutablePath);
 
     // Use cp.spawn / cp.exec for custom setup
-    cp.spawnSync(cliPath, ['--install-extension', 'vsls-contrib.codetour'], {
-      encoding: 'utf-8',
-      stdio: 'inherit',
-    });
+    cp.spawnSync(
+      cliPath,
+      [
+        '--install-extension',
+        'vsls-contrib.codetour',
+        '--extensions-dir',
+        extensionInstallPath,
+      ],
+      {
+        encoding: 'utf-8',
+        stdio: 'inherit',
+      }
+    );
 
     // Use cp.spawn / cp.exec for custom setup
-    cp.spawnSync(cliPath, ['--install-extension', 'humao.rest-client'], {
-      encoding: 'utf-8',
-      stdio: 'inherit',
-    });
+    cp.spawnSync(
+      cliPath,
+      [
+        '--install-extension',
+        'humao.rest-client',
+        '--extensions-dir',
+        extensionInstallPath,
+      ],
+      {
+        encoding: 'utf-8',
+        stdio: 'inherit',
+      }
+    );
 
     // Download VS Code, unzip it and run the integration test
     await runTests({
       vscodeExecutablePath,
       extensionDevelopmentPath,
       extensionTestsPath,
+      launchArgs: [
+        '--extensions-dir',
+        extensionInstallPath,
+        '--wait-for-extension-ready',
+      ],
     });
   } catch (err) {
     console.error('Failed to run tests');
