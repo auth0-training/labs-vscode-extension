02ee3164b4a2ba88fe99831a1ffa2bc62255ff5f
Bobby Johnson <bobby.johnson@gmail.com>
2021-07-15 13:17:39 -0700

    adding appsembler to url resolver

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -3,6 +3,9 @@ export function getUrlForPort(port: number) {
   if (process.env['CODESPACES']) {
     return `https://${process.env.CLOUDENV_ENVIRONMENT_ID}-${port}.apps.codespaces.githubusercontent.com`;
   }
+  if (process.env['INSTANCE_ID']) {
+    return `https://${port}-0-${process.env.INSTANCE_ID}.auth0-labs.appsembler.com`;
+  }
   // default to localhost
   return `http://localhost:${port}`;
 }
