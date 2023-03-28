d9aed6bba34aedb6f276059cf4f40b10b7cf0ed1
Bobby Johnson <bobby.johnson@auth0.com>
2023-03-28 10:43:59 -0700

    updating test runner for latest version of glob

--- tests/suite/index.ts
+++ tests/suite/index.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
 import * as Mocha from 'mocha';
-import { glob } from 'glob';
+import * as glob from 'glob';
 
 export function run(): Promise<void> {
   // Create the mocha test
@@ -11,22 +11,28 @@ export function run(): Promise<void> {
 
   const testsRoot = path.resolve(__dirname, '..');
 
-  return new Promise(async (c, e) => {
-    try {
-      const files = await glob('**/**.test.js', { cwd: testsRoot });
-      files.forEach((file: any) =>
-        mocha.addFile(path.resolve(testsRoot, file))
-      );
+  return new Promise((c, e) => {
+    glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
+      if (err) {
+        return e(err);
+      }
 
-      mocha.run((failures) => {
-        if (failures > 0) {
-          e(new Error(`${failures} tests failed.`));
-        } else {
-          c();
-        }
-      });
-    } catch (err) {
-      return e(err);
-    }
+      // Add files to the test suite
+      files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
+
+      try {
+        // Run the mocha test
+        mocha.run((failures) => {
+          if (failures > 0) {
+            e(new Error(`${failures} tests failed.`));
+          } else {
+            c();
+          }
+        });
+      } catch (err) {
+        console.error(err);
+        e(err);
+      }
+    });
   });
 }
