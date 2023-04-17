9f9197013786c6f4d1a34d57f21929c965fee975
Sara Kazemi <36528008+kazemicode@users.noreply.github.com>
2023-04-17 16:32:36 -0700

    PORT -> port

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,7 +1,7 @@
 export function getUrlForPort(port: number) {
   //if running in codespaces
   if (process.env['CODESPACES']) {
-    return `https://${process.env.CODESPACE_NAME}-${PORT}.${process.env.GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}`;
+    return `https://${process.env.CODESPACE_NAME}-${port}.${process.env.GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}`;
   }
   if (process.env['INSTANCE_ID']) {
     return `https://${port}-0-${process.env.INSTANCE_ID}.auth0-labs.appsembler.com`;
