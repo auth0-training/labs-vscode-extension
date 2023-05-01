5c5eac3c5051b562d13b690993304afd76fbc7df
Bobby Johnson <bobby.johnson@auth0.com>
2023-05-01 12:52:30 -0700

    fix async write issue

--- src/features/labs/writer.ts
+++ src/features/labs/writer.ts
@@ -6,7 +6,7 @@ const openTextDocument = vscode.workspace.openTextDocument;
 const fs = vscode.workspace.fs;
 
 export class LabEnvWriter {
-  constructor(private workspace: vscode.Uri) { }
+  constructor(private workspace: vscode.Uri) {}
 
   getExisting = async (uri: vscode.Uri) => {
     try {
@@ -15,43 +15,36 @@ export class LabEnvWriter {
 
       // read and parse file into name/value pairs
       return await readUriContents(uri)
-        .then(file => file.split('\n'))
-        .then(lines => lines.map(line => {
-          const pair = line.split('=');
-          return { name: pair[0], value: pair[1] };
-        }));
+        .then((file) => file.split('\n'))
+        .then((lines) =>
+          lines.map((line) => {
+            const pair = line.split('=');
+            return { name: pair[0], value: pair[1] };
+          })
+        );
     } catch {
       // return an empty array if file does not exist
       return [];
     }
-
-
   };
 
   writeAll = async (resolvers: Resolver[]) => {
-    resolvers.forEach(async (resolver) => {
+    for (const resolver of resolvers) {
       const uri = vscode.Uri.joinPath(
         this.workspace,
         resolver.getDirectory(),
         '.env'
       );
 
-
       const existingEnv = await this.getExisting(uri);
       const newEnv = resolver.resolveEnv(resolvers);
       const unique = [
-        ...new Map(
-          existingEnv.concat(newEnv).map(m => [m.name, m])
-        )
-          .values()
+        ...new Map(existingEnv.concat(newEnv).map((m) => [m.name, m])).values(),
       ];
 
-      const env = unique
-        .map((i) => `${i.name}=${i.value}`)
-        .join('\n');
-
+      const env = unique.map((i) => `${i.name}=${i.value}`).join('\n');
 
       await fs.writeFile(uri, Buffer.from(env, 'utf8'));
-    });
+    }
   };
 }
